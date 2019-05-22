edef4bd1dbf12c26d00ac8e50aac797f662e49bc
Amar Singh <nly@disroot.org>
2019-05-22 22:39:10 +0530

    guix: update emacs-shroud to 1.15-6

--- guix.scm
+++ guix.scm
@@ -1,33 +1,15 @@
 ;;; guix.scm --- Package description for emacs-shroud
-;; 
+
 ;; Filename: guix.scm
 ;; Description: Package description for emacs-shroud
 ;; Author: Amar Singh
 ;; Maintainer: Amar Singh<nly@disroot.org>
 ;; Created: Thu Feb  7 13:49:08 2019 (+0530)
-;; Version: 0.1
-;; Package-Requires: (emacs-s, emacs-f, shroud, gnupg)
-;; Last-Updated: 2019 Feb
-;;           By: 
-;;     Update #: 0
-;; URL: http://nly.info.tm:9000/emacs/shroud.el
-;; Doc URL: 
+;; Version: 1.15
+;; URL: http://git.nly.info.tm:9000/emacs-shroud.git
 ;; Keywords: shroud, password manager
-;; Compatibility: Gnu Emacs 26.1
-;; 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Commentary: 
-;; 
-;; Emacs interface for shroud password manager.
-;; 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Change Log:
-;; 
-;; 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;; Compatibility: Gnu Emacs 25
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
@@ -40,43 +22,52 @@
 ;; 
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-;; 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+
 ;;; Code:
-(use-modules (guix packages)
-             (guix download)
-             (guix build-system emacs)
-             ((guix licenses) #:prefix license:)
-             (gnu packages password-utils)
-             (gnu packages emacs)
-             (gnu packages emacs-xyz))
+
+(use-modules
+ (guix packages)
+ (guix download)
+ (guix git-download)
+ (gnu packages gnupg)
+ (guix build-system emacs)
+ ((guix licenses) #:prefix license:)
+ (gnu packages password-utils)
+ (gnu packages emacs)
+ (gnu packages emacs-xyz))
 
 (define-public emacs-shroud
   (package
    (name "emacs-shroud")
-   (version "0.1")
-   (source (origin
-            (method url-fetch)
-            (uri
-             (string-append "https://github.com/o-nly/" name "/archive/release-" version ".tar.gz"))
-            (sha256
-             (base32
-              "0jxfa2fa6r0ay9c6nf4xwz5vddb1v45cgaxkgdxxhck0l527i3yc"))))
+   (version "694c502")
+   (source
+    (origin
+     (method git-fetch)
+     (uri
+      (git-reference
+       (url "http://github.com/o-nly/emacs-shroud.git")
+       (commit version)))
+     (file-name
+      (git-file-name name version))
+     (sha256
+      (base32
+       "1c1nmyzsgwp1amlz7ns2i1gz54cp8ig352r4gyhplky15l2z798r"))))
    (build-system emacs-build-system)
-   
    (propagated-inputs
-    `(("emacs-f" ,emacs-f)
+    `(("shroud" ,shroud)
+      ("emacs-f" ,emacs-f)
+      ("emacs-dash" ,emacs-dash)
       ("emacs-s" ,emacs-s)
       ("emacs-bui" ,emacs-bui)
-      ("shroud" ,shroud)))
-   (home-page "http://nly.info.tm:9001/emacs/shroud.el")
-   (synopsis "Shroud (gpg) support for Emacs")
+      ("gnupg" ,gnupg)))
+   (home-page "http://git.nly.info.tm:9001/shroud.git")
+   (synopsis "Emacs interface for Shroud password manager")
    (description
-    "This package provides functions for working with shroud password manager.")
+    "This package provides functions for working with shroud
+password manager using Elisp, a reader for Shroud DB, and an Buffers
+User Interface for using shroud password database.")
    (license license:gpl3+)))
 
 emacs-shroud
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; guix.scm ends here
